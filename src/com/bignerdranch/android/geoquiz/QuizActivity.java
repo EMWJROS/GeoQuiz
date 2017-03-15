f5b640120fb804b561a5f575aa10fda83ca38423
J. H. Roslund <emwjros@gmail.com>
2017-03-15 20:42:18 +0100

    Add checkAnswer()

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -29,6 +29,20 @@ public class QuizActivity extends Activity {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
 		mQuestionTextView.setText(question);
 	}
+	
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		int messageResId = 0;
+		
+		if (userPressedTrue == answerIsTrue) {
+			messageResId = R.string.correct_toast;
+		} else {
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -41,9 +55,7 @@ public class QuizActivity extends Activity {
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this,
-					R.string.incorrect_toast,
-					Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			
 			}
 		});
@@ -51,9 +63,7 @@ public class QuizActivity extends Activity {
 		mFalseButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this,
-						R.string.correct_toast,
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 			
 			}
 		});
